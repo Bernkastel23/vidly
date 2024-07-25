d3ea2712bd77fe8607a894467afc4897aa266f08
Bernkastel <tehokceanz@gmail.com>
2024-07-25 11:48:01 +0200

    finished with [create, update, delete] and other misc fixes

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 {action-animation-comedy-crime-drama-experimental-fantasy-historical-horror-romance-science fiction-thriller-western-musical-war}
 
 2.get-create-update-delete
+app.get()
+app.post()
+app.put()
+app.delete()
 
 */
 
@@ -31,7 +35,7 @@ const genres = [
 	{id: 15, name:'war'},
 ]
 
-function validateCourse(course){
+function validateGenre(genre){
 	const schema = {
 		name: Joi.string().min(3).required()
 	};
@@ -55,18 +59,45 @@ app.get('/api/genres/:id', (req, res)=>{
 });
 
 //CREATE
+app.post('/api/genres', (req, res) => {
+	
+	const {error} = validateGenre(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	const genre = {
+		id: genres.length + 1,
+		name: req.body.name
+	};
+	genres.push(genre);
+	res.send(genre);
+});
 
 //UPDATE
+app.put('/api/genres/:id', (req, res) => {
+
+	const genre = genres.find(c => c.id === parseInt(req.params.id));
+	if(!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+	const {error} = validateGenre(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	genre.name = req.body.name;
+	res.send(genre);
+});
 
 //DELETE
+app.delete('/api/genres/:id', (req, res) => {
+
+	const genre = genres.find(c => c.id === parseInt(req.params.id));
+	if(!genre) return res.status(404).send('The genre with the given ID was not found.');
+
+	const index = genres.indexOf(genre);
+	genres.splice(index, 1);
+
+	res.send(genre);
+})
 
 //PORT
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
-/*
-app.get()
-app.post()
-app.put()
-app.delete()
-*/
