81683b8d88ed7025ec9c39d1d10775e990782990
Bernkastel <tehokceanz@gmail.com>
2024-08-08 18:17:43 +0200

    restructuring the app

--- routes/customers.js
+++ routes/customers.js
@@ -1,36 +1,7 @@
+const {Customer, validate} = require('../models/customer');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-const Joi = require("joi");
-
-const Customer = mongoose.model('Customer', new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		minlength: 2,
-		maxlength: 50
-	},
-	isGold: {
-		type: Boolean,
-		default: false
-	},
-	phone: {
-		type: String,
-		required: true,
-		minlength: 5,
-		maxlength: 50
-	}
-}));
-
-function validateCustomer(customer){
-	const schema = {
-		name: Joi.string().min(2).max(50).required(),
-		isGold: Joi.boolean(),
-		phone: Joi.string().min(5).max(50).required()
-	};
-
-	return Joi.validate(customer, schema);
-}
 
 router.get('/', async (req, res)=> {
 	const customers = await Customer.find().sort('name');
@@ -46,7 +17,7 @@ router.get('/:id', async (req, res)=>{
 //CREATE
 router.post('/', async (req, res) => {
 	
-	const {error} = validateCustomer(req.body);
+	const {error} = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
 	let customer = new Customer( {
@@ -62,7 +33,7 @@ router.post('/', async (req, res) => {
 
 //UPDATE
 router.put('/:id', async (req, res) => {
-	const {error} = validateCustomer(req.body);
+	const {error} = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
 	const customer = await Customer.findByIdAndUpdate(req.params.id, {
--- routes/genres.js
+++ routes/genres.js
@@ -1,7 +1,7 @@
+const {Genre, validate} = require("../models/genre");
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-const Joi = require("joi");
 
 /*const genres = [
 	{id: 1, name:'action'},
@@ -21,23 +21,6 @@ const Joi = require("joi");
 	{id: 15, name:'war'},
 ]*/
 
-const Genre = mongoose.model('Genre', new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		minlength: 3,
-		maxlength: 50
-	}
-}));
-
-function validateGenre(genre){
-	const schema = {
-		name: Joi.string().min(3).required()
-	};
-
-	return Joi.validate(genre, schema);
-}
-
 router.get('/', async (req, res)=> {
 	const genres = await Genre.find().sort('name');
 	res.send(genres);
@@ -52,7 +35,7 @@ router.get('/:id', async (req, res)=>{
 //CREATE
 router.post('/', async (req, res) => {
 	
-	const {error} = validateGenre(req.body);
+	const {error} = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
 	let genre = new Genre( {name: req.body.name} )
@@ -64,7 +47,7 @@ router.post('/', async (req, res) => {
 
 //UPDATE
 router.put('/:id', async (req, res) => {
-	const {error} = validateGenre(req.body);
+	const {error} = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
 	const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name}, {new: true});
