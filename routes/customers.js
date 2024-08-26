81bc849f506cc7cccd8dde17a46a57e0040f4eea
Bernkastel <tehokceanz@gmail.com>
2024-08-26 19:14:37 +0200

    adding auth to routes that modify data

--- routes/customers.js
+++ routes/customers.js
@@ -1,3 +1,4 @@
+const auth = require('../middleware/auth');
 const {Customer, validate} = require('../models/customer');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -15,7 +16,7 @@ router.get('/:id', async (req, res)=>{
 });
 
 //CREATE
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
 	
 	const {error} = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
@@ -32,7 +33,7 @@ router.post('/', async (req, res) => {
 });
 
 //UPDATE
-router.put('/:id', async (req, res) => {
+router.put('/:id', auth, async (req, res) => {
 	const {error} = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
@@ -48,7 +49,7 @@ router.put('/:id', async (req, res) => {
 });
 
 //DELETE
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
 	const customer = await Customer.findByIdAndDelete({_id: req.params.id});
 	if(!customer) return res.status(404).send('The customer with the given ID was not found.');
 
--- routes/movies.js
+++ routes/movies.js
@@ -1,3 +1,4 @@
+const auth = require('../middleware/auth');
 const {Movie, validate} = require('../models/movie');
 const {Genre} = require('../models/genre');
 const mongoose = require('mongoose');
@@ -16,7 +17,7 @@ router.get('/:id', async (req, res)=>{
 });
 
 //CREATE
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
 	
 	const {error} = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
@@ -40,7 +41,7 @@ router.post('/', async (req, res) => {
 });
 
 //UPDATE
-router.put('/:id', async (req, res) => {
+router.put('/:id', auth, async (req, res) => {
 	const {error} = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
@@ -63,7 +64,7 @@ router.put('/:id', async (req, res) => {
 });
 
 //DELETE
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
 	const movie = await Movie.findByIdAndDelete({_id: req.params.id});
 	if(!movie) return res.status(404).send('The movie with the given ID was not found.');
 
