c759b77763ff89fa547fc9b6eeb4951e07d88552
Bernkastel <tehokceanz@gmail.com>
2024-09-08 18:12:43 +0200

    first integration test

--- startup/logging.js
+++ startup/logging.js
@@ -1,6 +1,6 @@
 require('express-async-errors');
 const winston = require('winston');
-require('winston-mongodb');
+//require('winston-mongodb');
 
 module.exports = function() {
 	winston.exceptions.handle(
@@ -12,5 +12,5 @@ module.exports = function() {
 	});
 
 	winston.add(new winston.transports.File({filename: 'logfile.log'}));
-	winston.add(new winston.transports.MongoDB({db: 'mongodb://localhost/vidly', level: 'info'}));
+	//winston.add(new winston.transports.MongoDB({db: 'mongodb://localhost/vidly', level: 'info'}));
 }
